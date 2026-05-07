cae06bcdf43e0c3988b70fae6f33efafeba55ef8
jlinares05 <jlinaresb@unbosque.edu.co>
2026-05-06 19:44:53 -0500

    Trabajo clase

--- Navigation/src/screens/LoginPage/LoginPage.tsx
+++ Navigation/src/screens/LoginPage/LoginPage.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { AuthTemplate } from "../../components/templates";
 import { LoginForm } from "../../components/organisms";
-
-const handleLogin = () => {
-    console.log("Tap en iniciar sesión")
-}
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { AuthStackParamList } from "../../Routes";
 
 const LoginPage = () => {
+    const navigation = useNavigation<NativeStackNavigationProp<AuthStackParamList>>();
+
+    const handleLogin = () => {
+    console.log("Tap en iniciar sesión")
+    navigation.navigate("Login");
+    }
     return (
         <AuthTemplate
         title ="Login">
--- Navigation/src/screens/RegisterPage/RegisterPage.tsx
+++ Navigation/src/screens/RegisterPage/RegisterPage.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { AuthTemplate } from "../../components/templates";
 import { RegisterForm } from "../../components/organisms";
-
-const handleRegister = () => {
-    console.log("Tap en registro")
-}
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { AuthStackParamList } from "../../Routes";
 
 const RegisterPage = () => {
+    const navigation = useNavigation<NativeStackNavigationProp<AuthStackParamList>>();
+
+    const handleRegister = () => {
+    console.log("Tap en registro")
+    navigation.navigate("Register");
+    }
     return (
         <AuthTemplate
         title ="Register"
