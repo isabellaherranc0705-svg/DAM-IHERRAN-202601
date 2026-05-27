9f17ce2e56ba42d5a2b30b20d6e1c6283bb09780
Isabella Herran <isabella.herranc0705@gmail.com>
2026-05-27 15:51:11 -0500

    Replace usandoHooks with personal project

--- usandoHooks/App.tsx
+++ usandoHooks/App.tsx
@@ -1,54 +1,22 @@
-<<<<<<< HEAD
 import React from 'react';
-import { HomeScreen } from './src/screens/HomeScreen.tsx';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import CalculateScreen from './src/screens/CalculateScreen';
+import { colors } from './src/themes/colors';
 
-export default function App() {
-  return <HomeScreen />;
-}
-=======
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor={colors.background} />
+      <CalculateScreen />
+    </SafeAreaView>
   );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.background,
   },
 });
 
 export default App;
->>>>>>> f07c0ddc9b4a999a66a82e4f86eadadb2609d90f
